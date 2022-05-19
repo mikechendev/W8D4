781af1b37dd283b6c26c97cd3203ec00129b3625
Michael Chen <michael.chen.1120@gmail.com>
2022-05-19 14:22:39 -0400

    Rename and finish clock

---
+++ intro_callbacks_exercises/clock.js
@@ -0,0 +1,57 @@
+class Clock {
+  constructor() {
+    // 1. Create a Date object.
+    // 2. Store the hours, minutes, and seconds.
+    // 3. Call printTime.
+    // 4. Schedule the tick at 1 second intervals.
+
+    const today = new Date();
+
+    this.hours = today.getHours();
+    this.minutes = today.getMinutes();
+    this.seconds = today.getSeconds();
+
+    this.printTime();
+
+    setInterval(this._tick.bind(this), 1000);
+  }
+
+  printTime() {
+    // Format the time in HH:MM:SS
+    // Use console.log to print it.
+    let time = this.hours + ':' + this.minutes + ':' + this.seconds;
+    console.log(time);
+  }
+
+  _tick() {
+    // 1. Increment the time by one second.
+    // 2. Call printTime.
+    this.incrementSeconds();
+    this.printTime();
+  }
+
+  incrementSeconds () {
+    this.seconds += 1;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.incrementMinutes();
+    }
+  }
+
+  incrementMinutes () {
+    this.minutes += 1;
+    if (this.minutes === 60) {
+      this.minutes = 0;
+      this.incrementHours();
+    }
+  }
+
+  incrementHours () {
+    this.hours += 1;
+    if (this.hours === 24) {
+      this.hours = 0;
+    }
+  }
+}
+
+const clock = new Clock();
